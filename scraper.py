1434b6c75f50bae29919250b5e02157b94726637
Tyler Smith <tyler.savon.smith@gmail.com>
2024-08-07 03:51:19 -0600

    feat: function to scrape title, link, and points from news articles

--- scraper.py
+++ scraper.py
@@ -3,6 +3,20 @@ from bs4 import BeautifulSoup
 
 res = requests.get('https://news.ycombinator.com')
 soup = BeautifulSoup(res.text, 'html.parser')
-links = soup.select('.titleline')
+links = soup.select('.titleline > a')
 votes = soup.select('.score')
-print(votes[0])
+
+
+def create_custom_hn(links, votes):
+    hn = []
+    for index, item in enumerate(links):
+        title = links[index].getText()
+        href = links[index].get('href', None)
+        hn.append({'title': title,
+                   'link': href})
+        points = votes[index].getText()
+        print(points)
+    return hn
+
+
+create_custom_hn(links, votes)
